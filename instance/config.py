34c19ab1336a2b8af83cfaab948809e3b28c4302
Joachim Oforma, Igwebuike <joachimigwebuike@gmail.com>
2024-06-24 12:48:35 -0800

    Finial bootcamp project

---
+++ instance/config.py
@@ -0,0 +1,14 @@
+ADMIN_EMAIL = 'joachim@gmail.com'
+SECRET_KEY = 'the^Power#of6%g#@'
+APP_NAME = 'careerconnect'
+DEBUG = True
+ENV = 'production'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root@localhost/careerconnect'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PASSWORD = ""
+MAIL_USERNAME = ""
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USE_TSL = False
